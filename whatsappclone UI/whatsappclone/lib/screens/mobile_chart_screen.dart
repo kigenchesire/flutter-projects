91b05277833978d63e82cdd066a715cf636a35e9
kigen chesire <ekchesire10@gmail.com>
2023-10-01 12:48:08 +0300

    chnages

--- whatsappclone UI/whatsappclone/lib/screens/mobile_chart_screen.dart
+++ whatsappclone UI/whatsappclone/lib/screens/mobile_chart_screen.dart
@@ -2,15 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:whatsappclone/info.dart';
 import 'package:whatsappclone/widgets/colors.dart';
 
-class  MobileChatScreen extends StatelessWidget {
-  const MobileChatScreen ({super.key});
+class MobileChatScreen extends StatelessWidget {
+  const MobileChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: appBarColor,
-        title: Text(info[0]['name'].toString(),),
+        title: Text(
+          info[0]['name'].toString(),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.video_call),),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.call),),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert),),
+        ],
+      ),
+      body: Column(
+        
       ),
     );
   }
